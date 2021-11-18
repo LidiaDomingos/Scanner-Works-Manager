9491bd7797ca4798b57d6243a5dcdfbae9f42feb
leticiacb1 <leticiacb1@al.insper.edu.br>
2021-11-18 04:50:04 -0300

    Finalizando estrutura componente tabela.

--- Frontend/components/Historico.js
+++ Frontend/components/Historico.js
@@ -20,31 +20,61 @@ export default function Historico(props) {
                 value={searchQuery}
             />
 
-            <DataTable>
+            <DataTable style = {styles.tabela}>
 
                 <DataTable.Header>
-                    <DataTable.Title>Dessert</DataTable.Title>
-                    <DataTable.Title numeric>Calories</DataTable.Title>
-                    <DataTable.Title numeric>Fat</DataTable.Title>
+                    <DataTable.Title>ID</DataTable.Title>
+                    <DataTable.Title numeric>Data</DataTable.Title>
+                    <DataTable.Title numeric>Hora</DataTable.Title>
+                    <DataTable.Title numeric>Usuário</DataTable.Title>
                 </DataTable.Header>
 
                 <DataTable.Row>
-                    <DataTable.Cell>Frozen yogurt</DataTable.Cell>
-                    <DataTable.Cell numeric>159</DataTable.Cell>
-                    <DataTable.Cell numeric>6.0</DataTable.Cell>
+                    <DataTable.Cell>Madeira</DataTable.Cell>
+                    <DataTable.Cell numeric>15/10/2021</DataTable.Cell>
+                    <DataTable.Cell numeric>14:30</DataTable.Cell>
+                    <DataTable.Cell numeric>user1</DataTable.Cell>
                 </DataTable.Row>
 
                 <DataTable.Row>
-                    <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-                    <DataTable.Cell numeric>237</DataTable.Cell>
-                    <DataTable.Cell numeric>8.0</DataTable.Cell>
+                    <DataTable.Cell>Martelo</DataTable.Cell>
+                    <DataTable.Cell numeric>22/03/2021</DataTable.Cell>
+                    <DataTable.Cell numeric>4:30</DataTable.Cell>
+                    <DataTable.Cell numeric>user2</DataTable.Cell>
+                </DataTable.Row>
+
+                <DataTable.Row>
+                    <DataTable.Cell>Cano</DataTable.Cell>
+                    <DataTable.Cell numeric>07/10/2021</DataTable.Cell>
+                    <DataTable.Cell numeric>19:15</DataTable.Cell>
+                    <DataTable.Cell numeric>user3</DataTable.Cell>
+                </DataTable.Row>
+
+                <DataTable.Row>
+                    <DataTable.Cell>Madeira</DataTable.Cell>
+                    <DataTable.Cell numeric>04/09/2021</DataTable.Cell>
+                    <DataTable.Cell numeric>15:50</DataTable.Cell>
+                    <DataTable.Cell numeric>user4</DataTable.Cell>
+                </DataTable.Row>
+
+                <DataTable.Row>
+                    <DataTable.Cell>Ferro</DataTable.Cell>
+                    <DataTable.Cell numeric>25/11/2020</DataTable.Cell>
+                    <DataTable.Cell numeric>7:30</DataTable.Cell>
+                    <DataTable.Cell numeric>user5</DataTable.Cell>
+                </DataTable.Row>
+
+                <DataTable.Row>
+                    <DataTable.Cell>Cimento</DataTable.Cell>
+                    <DataTable.Cell numeric>13/11/2021</DataTable.Cell>
+                    <DataTable.Cell numeric>12:30</DataTable.Cell>
+                    <DataTable.Cell numeric>user6</DataTable.Cell>
                 </DataTable.Row>
 
                 <DataTable.Pagination
                     page={1}
                     numberOfPages={3}
                     label="1-2 of 6"
-                    itemsPerPage={5}
                     optionsLabel={'Rows per page'}
                 />
                 </DataTable>
