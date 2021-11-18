d681980f95b7b05fa51beed034a183db79f72573
leticiacb1 <leticiacb1@al.insper.edu.br>
2021-11-18 03:31:05 -0300

    Subindo inputs ultimo registro.

--- Frontend/components/Informacoes.js
+++ Frontend/components/Informacoes.js
@@ -2,7 +2,7 @@ import React , { useState} from 'react';
 
 import { View , ScrollView } from 'react-native';
 
-import { TextInput , List} from 'react-native-paper';
+import { TextInput , List , Divider} from 'react-native-paper';
 
 import { DropDown, DateTimePicker} from '../lib';
 
@@ -13,22 +13,42 @@ export default function Informacoes(props) {
     const [date, setDate] = useState(new Date());
     const [nowTime, setNowTime] = useState(new Date());
 
+    const [lastDate, setLastDate] = useState(new Date());
+    const [lastTime, setLastTime] = useState(new Date());
+
     return (
         <ScrollView style={styles.container}>
 
-            <List.Section style={styles.Geral}>
+            <List.Section style={styles.geral}>
 
                  <List.Subheader>Geral</List.Subheader>
 
                 <TextInput style={styles.input} label="ID"/>
                 <TextInput style={styles.input} label="Local"/>
                 <TextInput style={styles.input} label="Usuario"/>
-                <View style={styles.now}>
+
+                <View style={styles.tempo}>
                     <DateTimePicker type="date" style={styles.inputDate} label="Data" value={date} setValue={setDate} />
                     <DateTimePicker type="time" style={styles.inputTime} label="Hora" value={nowTime} setValue={setNowTime} />
                 </View>
 
             </List.Section>
+
+            <Divider />
+
+            <List.Section style={styles.ultimoRegistro}>
+
+                 <List.Subheader>Último registro</List.Subheader>
+ 
+                <View style={styles.tempo}>
+                    <DateTimePicker type="date" style={styles.inputDate} label="Data" value={lastDate} setValue={setLastDate} />
+                    <DateTimePicker type="time" style={styles.inputTime} label="Hora" value={lastTime} setValue={setLastTime} />
+                </View>
+
+            </List.Section>
+
+            <Divider />
+
 
         </ScrollView>
     );
