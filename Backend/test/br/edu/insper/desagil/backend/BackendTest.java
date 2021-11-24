da7e323ab59a403b530ca67e1639829b36b333e7
leticiacb1 <leticiacb1@al.insper.edu.br>
2021-11-24 20:04:12 -0300

    Subindo BackendTest.

--- Backend/test/br/edu/insper/desagil/backend/BackendTest.java
+++ Backend/test/br/edu/insper/desagil/backend/BackendTest.java
@@ -1,5 +1,5 @@
 package br.edu.insper.desagil.backend;
 
 public class BackendTest {
-	public static final String URL = "http://___.___.___.___:____";
+	public static final String URL = "http://5e3b-186-232-61-4.ngrok.io";
 }
