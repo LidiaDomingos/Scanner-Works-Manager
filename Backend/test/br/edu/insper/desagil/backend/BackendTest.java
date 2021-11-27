88415615482fbad5659b386fef25934235e9455b
niveaabreu <niveaadl@al.insper.edu.br>
2021-11-27 00:20:55 -0300

    Corrige erros de merge conflict

--- Backend/test/br/edu/insper/desagil/backend/BackendTest.java
+++ Backend/test/br/edu/insper/desagil/backend/BackendTest.java
@@ -1,9 +1,5 @@
 package br.edu.insper.desagil.backend;
 
 public class BackendTest {
-<<<<<<< HEAD
 	public static final String URL = "http://192.168.15.2:8080";
-=======
-	public static final String URL = "http://1c9a-2804-431-c7c1-b3f2-6943-ec4f-e628-2a4.ngrok.io";
->>>>>>> TelasIniciais_Leticia
 }
